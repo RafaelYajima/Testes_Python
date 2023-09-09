7d6bf9569215687fd63d598553ceb1d6b3a79703
Rafael Yajima <rafaelyajima2@gmail.com>
2023-09-09 10:51:35 -0300

    Meu arquivo "hello world"

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+nome = "Rafael"
+profissao = "Estudante"
+
+print(f"Este é o primeiro programa de {nome}")
+print("Eu sou", profissao, "e decide trilhar os caminhos do python")
+
+resposta = input("Você esta pronto para vir comigo? (S/N)")
