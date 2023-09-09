ee8cdc3ef38982fb4da7f97df11dee99fc7ba032
Rafael Yajima <rafaelyajima2@gmail.com>
2023-09-09 12:59:05 -0300

    Conversor de tempo

---
+++ ConversorDeTemperatura.py
@@ -0,0 +1,12 @@
+print("============================================")
+print("=-=-=-=-= Conversor de temperatura =-=-=-=-=")
+print("============================================")
+
+c = float(input("Digite a temperatura em Celcius:"))
+f = 32 + (9/5)*c
+k = c +273
+
+print("============================================")
+
+print("A temperatura em fahrenheit é:",f,"°f")
+print("A temperatura em kelvin é:",k,"k")
