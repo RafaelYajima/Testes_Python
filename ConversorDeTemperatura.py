1b0946f416a58fcc9df1f3ea152a1d2accb04711
Rafael Yajima <rafaelyajima2@gmail.com>
2023-09-09 14:16:51 -0300

    Conversor de temperatura

--- ConversorDeTemperatura.py
+++ ConversorDeTemperatura.py
@@ -5,7 +5,7 @@ print("=-=-=-=-= Conversor de temperatura =-=-=-=-=")
 print("============================================")
 
 c = float(input("Digite a temperatura em Celcius:"))
-f = 32 + (9/5)*c
+f = 32 + (1.8*c)
 k = c +273
 
 print("============================================")
