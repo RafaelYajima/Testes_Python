3b48edec854215907fbe7b41bafe86a6d3caba9b
Rafael Yajima <rafaelyajima2@gmail.com>
2023-09-09 14:24:57 -0300

    Calculadora de juros simples

--- CalculadorDeJuros.py
+++ CalculadorDeJuros.py
@@ -5,11 +5,11 @@ print ("=-=-=-=-= Calculadora de juros simples =-=-=-=-=")
 print("=================================================")
 
 p = float(input("Digite o valor principal:"))
-tanual = float(input("Digite a taxa de juros anual:"))
+tanual = float(input("Digite a taxa de juros anual em porcentagem:"))
 tanos = float(input("Digite o tempo em anos:"))
 
 print("=================================================")
 
-montante = p + (p * tanual * tanos)
+montante = p + (p * (tanual/100) * tanos)
 
 print("Seu montante é;",montante)
