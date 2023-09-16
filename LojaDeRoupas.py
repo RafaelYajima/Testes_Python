78339af561a1e352aab437db97e46ed22df7b557
Rafael Yajima <rafaelyajima2@gmail.com>
2023-09-16 12:03:05 -0300

    Loja de roupas com juros

--- LojaDeRoupas.py
+++ LojaDeRoupas.py
@@ -5,30 +5,23 @@ modo = float(input("Digite o modo de pagamento 1(A vista), 2(A prazo):"))
 
 if modo == 1:
     print("Pagamento a vista com deconto")
- 
     if produto > 500:
         result = produto * 0.90
     elif produto > 1000 :
         result = produto * 0.80
-
     print("-----------------------------------------")
     print("O total a pagar é:",result,"reais")
-
 if modo == 2:
     print("Pagamento feito a prazo")
-
     if produto >= 800 :
         print("Pode ser parcelado acima de 5x")
     else:
         print("Nao podera fazer parcelas")
-
     print("-----------------------------------------")
-
     print("Caso seja parcelado em mais do que 10x possui juros")
     parc = float(input("Digite quantas parcelas ira fazer até 18x: "))
 
     if parc > 10 and parc < 19:
-    
         if parc == 11 :
             juros = produto * 0.05
             parcela = (produto / 11) + juros
